d7d1f41e86678238cb18db02bb7f5548229c9b2b
prog2448 <peddisaivivek999@gmail.com>
2022-06-07 01:57:39 +0000

    Final AWS Commit

--- preprocess/__init__.py
+++ preprocess/__init__.py
@@ -166,11 +166,32 @@ class Preprocess:
         
         os.system(f'cp -r {src_dir}/* {dest_dir}' )
 
+
+src_dir = '/home/ubuntu/audio_transformer/data/train_2'
+dest_dir = '/home/ubuntu/audio_transformer/data/train'
+
+for item in out_json:
+    os.system(f'cp {src_dir}/{item} {dest_dir}' )
+# with open('/home/ubuntu/audio_transformer/data/train/metadata.jsonl', 'w') as outfile:
+#     for entry in JSON_file:
+#         json.dump(entry, outfile)
+#         outfile.write('\n')
+
+#     def create_a_100_sample(self):
+#         print('Creating 100 data samples for ViT')
+#         df = pd.read_csv(f'{self.data_path}/UrbanSound8K.csv')
+#         df['file_name'] = df.apply(lambda x: x["slice_file_name"][:-3] + f'png' , axis = 1)
+        
+
+
     
 
 
+# def check(x):
+#     return True if x in out_json else False
 
-
+# df['check'] = df.apply(lambda x: check(x['file_name']),axis=1)
+# df = df[df['check']==True]
 
 # def process_image(img_name):
 #     img = Image.open(img_name)
